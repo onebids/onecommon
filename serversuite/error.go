05aa8b521203e8013abd3c798143e312a01bac44
eliohn <yihui86.huang@gmail.com>
2024-10-27 17:14:10 +0800

    refactor(error): 重构错误处理逻辑
    - 更新 ServerErrorHandler 函数，增加 err_no 参数
    - 使用 err_no 作为错误码创建 TransError
    - 注释掉原有的 GetErrorCode 相关逻辑

--- serversuite/error.go
+++ serversuite/error.go
@@ -5,21 +5,23 @@ import (
 	"errors"
 	"github.com/cloudwego/kitex/pkg/kerrors"
 	"github.com/cloudwego/kitex/pkg/remote"
+	"github.com/onebids/onecommon/consts/errno"
 )
 
 // convert errors that can be serialized
-func ServerErrorHandler(ctx context.Context, err error) error {
+func ServerErrorHandler(ctx context.Context, err error, err_no errno.Err) error {
 	// if you want get other rpc info, you can get rpcinfo first, like `ri := rpcinfo.GetRPCInfo(ctx)`
 	// for example, get remote address: `remoteAddr := rpcinfo.GetRPCInfo(ctx).From().Address()`
 
 	if errors.Is(err, kerrors.ErrBiz) {
 		err = errors.Unwrap(err)
 	}
-	if errCode, ok := GetErrorCode(err); ok {
-		// for Thrift、KitexProtobuf
-		return remote.NewTransError(errCode, err)
-	}
-	return err
+	return remote.NewTransError(int32(err_no), err)
+	//if errCode, ok := GetErrorCode(err); ok {
+	//	// for Thrift、KitexProtobuf
+	//	return remote.NewTransError(errCode, err)
+	//}
+	//return err
 }
 
 func GetErrorCode(err error) (int32, bool) {
