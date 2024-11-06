5b4a5b096ab6d3f5507d154f62040a89fbc26a28
eliohn <yihui86.huang@gmail.com>
2024-11-07 00:34:31 +0800

    feat(tools): 添加 GetCtxValue 函数
    - 新增 GetCtxValue 函数，用于从 context 中获取指定键的值
    - 如果指定键不存在，则返回默认值- 该函数简化了从 context 中获取值的逻辑，提高了代码可读性和复用性

---
+++ tools/ctx_tool.go
@@ -0,0 +1,15 @@
+package tools
+
+import (
+	"context"
+	"github.com/bytedance/gopkg/cloud/metainfo"
+)
+
+func GetCtxValue(ctx context.Context, key string, defaultValue string) string {
+	lang, ok1 := metainfo.GetValue(ctx, key)
+	if ok1 {
+		return lang
+	} else {
+		return defaultValue
+	}
+}
