6ec253b1a7c77f8d6d7fb05893d673b153702167
eliohn <yihui86.huang@gmail.com>
2024-12-15 12:28:47 +0800

    feat(tools): 添加获取账户ID和语言的上下文工具函数- 新增 GetAccountId 和 GetLanguage 函数 - 使用常量定义账户ID和语言的上下文键 - 优化了上下文值的获取逻辑，提高代码复用性

--- tools/ctx_tool.go
+++ tools/ctx_tool.go
@@ -3,6 +3,7 @@ package tools
 import (
 	"context"
 	"github.com/bytedance/gopkg/cloud/metainfo"
+	"github.com/onebids/onecommon/consts"
 )
 
 func GetCtxValue(ctx context.Context, key string, defaultValue string) string {
@@ -17,4 +18,11 @@ func GetCtxValue(ctx context.Context, key string, defaultValue string) string {
 func SetCtxValue(ctx context.Context, key string, value string) context.Context {
 	ctx = metainfo.WithValue(ctx, key, value)
 	return ctx
+}
+
+func GetAccountId(ctx context.Context) string {
+	return GetCtxValue(ctx, consts.AccountID, "")
+}
+func GetLanguage(ctx context.Context) string {
+	return GetCtxValue(ctx, consts.Language, consts.DefaultLanguage)
 }
