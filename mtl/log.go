57dd6a7b9570c9b43e5d2ab037414e2dd446b203
eliohn <yihui86.huang@gmail.com>
2025-01-18 15:46:52 +0800

    feat(log): 添加链路追踪信息到日志
    - 在日志中添加 trace_id 和 span_id 字段，用于链路追踪
    - 使用环境变量控制日志格式（开发环境为控制台格式，生产环境为 JSON 格式）
    - 优化日志的异步写入逻辑，提高性能

--- mtl/log.go
+++ mtl/log.go
@@ -15,9 +15,11 @@
 package mtl
 
 import (
+	"context"
 	"io"
-	"os"
 	"time"
+
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/server"
@@ -29,29 +31,34 @@ import (
 func InitLog(ioWriter io.Writer) {
 	var opts []kitexzap.Option
 	var output zapcore.WriteSyncer
-	if os.Getenv("GO_ENV") != "online" {
-		opts = append(opts, kitexzap.WithCoreEnc(zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())))
-		output = zapcore.AddSync(ioWriter)
-	} else {
-		opts = append(opts, kitexzap.WithCoreEnc(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())))
-		// async log
-		output = &zapcore.BufferedWriteSyncer{
-			WS:            zapcore.AddSync(ioWriter),
-			FlushInterval: time.Minute,
-		}
+	//if os.Getenv("GO_ENV") != "online" {
+	//	opts = append(opts, kitexzap.WithCoreEnc(zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())))
+	//	output = zapcore.AddSync(ioWriter)
+	//} else {
+	opts = append(opts, kitexzap.WithCoreEnc(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())))
+	// async log
+	output = &zapcore.BufferedWriteSyncer{
+		WS:            zapcore.AddSync(ioWriter),
+		FlushInterval: time.Minute,
 	}
+	//}
 	opts = append(opts, kitexzap.WithRecordStackTraceInSpan(true))
-	//opts = append(opts, kitexzap.WithCustomFields(
-	//	func(ctx context.Context) []zapcore.Field {
-	//		span := trace.SpanFromContext(ctx)
-	//		if !span.SpanContext().IsValid() {
-	//			return nil
-	//		}
-	//		return []zapcore.Field{
-	//			zap.String("trace_id", span.SpanContext().TraceID().String()),
-	//			zap.String("span_id", span.SpanContext().SpanID().String()),
-	//		}
-	//	}))
+	opts = append(opts, kitexzap.WithCustomFields(
+		func(ctx context.Context) []zapcore.Field {
+			span := trace.SpanFromContext(ctx)
+			if !span.SpanContext().IsValid() {
+				return []zapcore.Field{}
+			}
+			traceID := span.SpanContext().TraceID().String()
+			spanID := span.SpanContext().SpanID().String()
+			if traceID == "" || spanID == "" {
+				return []zapcore.Field{}
+			}
+			return []zapcore.Field{
+				zap.String("trace_id", traceID),
+				zap.String("span_id", spanID),
+			}
+		}))
 
 	server.RegisterShutdownHook(func() {
 		output.Sync() //nolint:errcheck
