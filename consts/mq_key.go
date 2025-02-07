f868f5a1b3ae33f34c23f56291fc0bfd8dd0bfa6
eliohn <yihui86.huang@gmail.com>
2025-02-07 13:29:39 +0800

    const(mq): 更新 MQ 相关常量
    - 移除旧的 MQ 常量定义
    - 新增多个 MQ队列和键的常量- 包括幸运抽奖、订单过期和开奖活动相关的队列和键

--- consts/mq_key.go
+++ consts/mq_key.go
@@ -4,4 +4,19 @@ const (
 	MQ_BOT_NOTIFY_KEY    = "BOT_NOTIFY"
 	MQ_BOT_NOTIFY_ROUNTE = "BOT_NOTIFY_ROUTE"
 	MQ_BOT_NOTIFY_QUEUE  = "BOT_NOTIFY_QUEUE"
+
+	MqPopQueue       = "LuckeyIdCheckQueue"
+	MqPopDelayPopKey = "DelayPopKeyV2"
+	MqPopRoutingKey  = "MqPopRoutingKey"
+
+	// 订单过期队列
+	MqOrderExpiredQueue      = "OrderExpiredQueue"
+	MqOrderExpiredKey        = "OrderExpiredKey"
+	MqOrderExpiredRoutingKey = "OrderExpiredRoutingKey"
+	// 开奖活动队列
+	MqActivityQueue      = "OpenActivityQueue"
+	MqActivityKey        = "OpenActivityKey"
+	MqActivityRoutingKey = "OpenActivityRoutingKey"
+
+	RdmOrderExpiredKey = "OrderExpired:%s"
 )
